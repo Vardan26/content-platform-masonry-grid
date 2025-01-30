3d98cc5233a707929eb5627f8cedcb7086be6aa8
Vardan Shahbazyan <vardan.shahbazyan.95@gmail.com>
2025-01-30 10:28:27 +0400

    Add Error boundary component based on ReactErrorBoundary

---
+++ src/components/errorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ErrorBoundary as ReactErrorBoundary } from "react-error-boundary";
+
+const ErrorFallback = ({
+  error,
+  resetErrorBoundary,
+}: {
+  error: Error;
+  resetErrorBoundary: () => void;
+}) => (
+  <div role="alert">
+    <p>Something went wrong:</p>
+    <pre>{error.message}</pre>
+    <button onClick={resetErrorBoundary}>Try again</button>
+  </div>
+);
+
+export const ErrorBoundary: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <ReactErrorBoundary FallbackComponent={ErrorFallback}>
+      {children}
+    </ReactErrorBoundary>
+  );
+};
